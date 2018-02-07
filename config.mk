6d06eabc045ee4fb67bfff3482bdb213017ed0cd
Patrick Steinhardt <ps@pks.im>
2018-02-07 00:16:29 +0000

    sb-cat: greatly simplify parsing index

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ BINDIR := $(DESTDIR)$(PREFIX)/bin
 
 CC ?= gcc
 
-CFLAGS   := $(shell pkg-config --cflags libsodium) -Wall -Wextra -pedantic -std=c90 ${CFLAGS}
+CFLAGS   := $(shell pkg-config --cflags libsodium) -Wall -Wextra -pedantic -std=c90 -D_POSIX_C_SOURCE=200809L ${CFLAGS}
 LDFLAGS  := $(shell pkg-config --libs libsodium) ${LDFLAGS}
--- sb-cat.c
+++ sb-cat.c
@@ -25,32 +25,10 @@
 #include "config.h"
 #include "common.h"
 
-#define MIN(x, y) ((x) <= (y) ? (x) : (y))
-
-static const char *find_last_line(const char *string)
-{
-    const char *end = string + strlen(string) - 1;
-
-    while (*end == '\n')
-        end--;
-
-    while (end[-1] != '\n' && end > string)
-        end--;
-
-    if (end == string)
-        return NULL;
-
-    return end;
-}
-
-static int parse_trailer(unsigned char *hash_out, size_t *datalen_out, const char *chain)
+static int parse_trailer(unsigned char *hash_out, size_t *datalen_out, const char *trailer)
 {
     char hash[HASH_LEN * 2];
-    const char *trailer;
     int i;
-
-    if ((trailer = find_last_line(chain)) == NULL)
-        die("Invalid input without trailer");
 
     if (*trailer != '>')
         die("Last line is not a trailer line");
@@ -75,50 +53,14 @@ static int parse_trailer(unsigned char *hash_out, size_t *datalen_out, const cha
     return 0;
 }
 
-static char *read_index(unsigned char *hash_out, size_t *size_out)
-{
-    unsigned char buf[1024];
-    char *chain = NULL;
-    ssize_t bytes;
-    size_t total = 0;
-
-    while ((bytes = read_bytes(STDIN_FILENO, buf, sizeof(buf))) > 0) {
-        chain = realloc(chain, total + bytes + 1);
-        memcpy(chain + total, buf, bytes);
-        total += bytes;
-        chain[total] = '\0';
-    }
-
-    if (bytes < 0)
-        die_errno("Unable to read from stdin");
-
-    if (parse_trailer(hash_out, size_out, chain) < 0)
-        die("Unable to parse trailer");
-
-    return chain;
-}
-
-static int read_block(unsigned char *out, size_t outlen, int storefd, char *hash)
-{
-    int fd;
-
-    if ((fd = open_block(storefd, hash, 0)) < 0)
-        die_errno("Unable to open block '%s'", hash);
-
-    if (read_bytes(fd, out, outlen) != (ssize_t) outlen)
-        die_errno("Unable to read block '%s'", hash);
-
-    close(fd);
-    return 0;
-}
-
 int main(int argc, char *argv[])
 {
     crypto_generichash_state *state = malloc(crypto_generichash_statebytes());
-    unsigned char trailer_hash[HASH_LEN], computed_hash[HASH_LEN];
+    unsigned char expected_trailer[HASH_LEN], computed_hash[HASH_LEN];
     unsigned char *block = malloc(BLOCK_LEN);
-    char *chain = NULL, *haystack, *hash;
-    size_t data_len;
+    char *line = NULL;
+    ssize_t linelen;
+    size_t total = 0, n = 0, expected_len;
     int storefd;
 
     if (argc != 2)
@@ -127,48 +69,54 @@ int main(int argc, char *argv[])
     if ((storefd = open(argv[1], O_DIRECTORY)) < 0)
         die_errno("Unable to open storage '%s'", argv[1]);
 
-    if ((chain = read_index(trailer_hash, &data_len)) == NULL)
-        die("Unable to read index");
-
     if (crypto_generichash_init(state, NULL, 0, HASH_LEN) < 0)
         die("Unable to initialize hashing state");
 
-    haystack = chain;
-    while ((hash = strtok(haystack, "\n")) != NULL) {
-        unsigned char line_hash[HASH_LEN];
-        size_t blocklen = MIN(data_len, BLOCK_LEN);
+    while ((linelen = getline(&line, &n, stdin)) > 0) {
+        ssize_t blocklen;
+        int blockfd;
 
-        if (*hash == '>')
+        if (*line == '>')
             break;
 
-        if (data_len == 0)
-            die("More lines, but all data read");
+        if (line[linelen - 1] == '\n')
+            line[--linelen] = '\0';
 
-        if (hex2bin(line_hash, sizeof(line_hash), hash, strlen(hash)) < 0)
-            die("Unable to decode hash");
+        if (strspn(line, "0123456789abcdef") != 2 * HASH_LEN)
+            die("Invalid index hash '%s'", line);
 
-        if (read_block(block, blocklen, storefd, hash) < 0)
-            die_errno("Unable to read block '%s'", hash);
+        if ((blockfd = open_block(storefd, line, 0)) < 0)
+            die_errno("Unable to open block '%s'", line);
+
+        if ((blocklen = read_bytes(blockfd, block, BLOCK_LEN)) <= 0)
+            die_errno("Unable to read block '%s'", line);
 
         if (crypto_generichash_update(state, block, blocklen) < 0)
             die("Unable to update hash");
 
         if (write_bytes(STDOUT_FILENO, block, blocklen) < 0)
-            die_errno("Unable to write block '%s'", hash);
+            die_errno("Unable to write block '%s'", line);
 
-        data_len -= blocklen;
-        haystack = NULL;
+        total += blocklen;
+        close(blockfd);
     }
 
-    if (data_len)
-        die("Premature end of chain");
+    if (linelen < 0 && !feof(stdin))
+        die_errno("Unable to read index");
+
+    if ((parse_trailer(expected_trailer, &expected_len, line)) < 0)
+        die("Unable to read index");
 
     if (crypto_generichash_final(state, computed_hash, sizeof(computed_hash)) < 0)
         die("Unable to finalize hash");
 
-    if (memcmp(computed_hash, trailer_hash, sizeof(computed_hash)))
-        die("Trailer hash does not match computed hash");
+    if (total != expected_len)
+        die("Size mismatch");
 
+    if (memcmp(computed_hash, expected_trailer, sizeof(computed_hash)))
+        die("Hash mismatch");
+
+    free(line);
     free(state);
     free(block);
 
