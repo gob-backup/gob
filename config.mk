27a2b414e6389700703989d73af8342da1982fc7
Patrick Steinhardt <ps@pks.im>
2018-08-31 13:41:15 +0200

    config.mk: move BSD/POSIX defines to CPPFLAGS
    The CPPFLAGS are generally used for things like defines or includes
    instead of using the CFLAGS variable. This is so that the CFLAGS
    variable can be overridden, e.g. to include additional diagnostics or
    the optimization level, while still maintaining the set of flags
    necessary to build.
    
    Fix the config.mk file to adhere to this rule by moving both the
    "-D_BSD_SOURCE" and "-D_POSIX_C_SOURCE" defines to the CPPFLAGS
    variable.

--- config.mk
+++ config.mk
@@ -6,6 +6,6 @@ MANDIR := $(DESTDIR)$(PREFIX)/share/man/man1
 
 CC ?= gcc
 
-CFLAGS   := -Wall -Wextra -pedantic -std=c90 -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L ${CFLAGS}
-CPPFLAGS := -DGOB_VERSION=${VERSION} $(shell pkg-config --cflags libsodium) ${CPPFLAGS}
+CFLAGS   := -Wall -Wextra -pedantic -std=c90 ${CFLAGS}
+CPPFLAGS := -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DGOB_VERSION=${VERSION} $(shell pkg-config --cflags libsodium) ${CPPFLAGS}
 LDLIBS   := $(shell pkg-config --libs libsodium) ${LDLIBS}
