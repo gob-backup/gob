d96e52e3554526cb29bb0c5f5fe744fc99abaf69
Patrick Steinhardt <ps@pks.im>
2018-08-31 13:42:51 +0200

    config.mk: avoid amending build flags
    To build the project with a custom set of build flags, one can usually
    either pass in the CFLAGS variable via an environment variable or by
    specifying it after the make(1) command. The assumption is that this
    will then completely override the set of build flags provided by the
    project itself.
    
    Instead of replacing the CFLAGS, we instead amend the passed in flags to
    our usual set of flags. As previously explained, this is the wrong thing
    to do. Fix this by instead using conditional assignment of our own
    values, such that we will never clobber provided CFLAGS.

--- config.mk
+++ config.mk
@@ -4,8 +4,6 @@ PREFIX ?= /usr/local
 BINDIR := $(DESTDIR)$(PREFIX)/bin
 MANDIR := $(DESTDIR)$(PREFIX)/share/man/man1
 
-CC ?= gcc
-
-CFLAGS   := -Wall -Wextra -pedantic -std=c90 ${CFLAGS}
-CPPFLAGS := -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DGOB_VERSION=${VERSION} $(shell pkg-config --cflags libsodium) ${CPPFLAGS}
-LDLIBS   := $(shell pkg-config --libs libsodium) ${LDLIBS}
+CFLAGS   ?= -Wall -Wextra -pedantic -std=c90
+CPPFLAGS := -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DGOB_VERSION=${VERSION} $(shell pkg-config --cflags libsodium)
+LDLIBS   := $(shell pkg-config --libs libsodium)
