9118037a7fcf42245e0702a9e0db7ab6aa0af5be
Patrick Steinhardt <ps@pks.im>
2019-11-19 12:31:51 +0100

    gob-chunk: remove shadowing variable declaration
    There is a shadowing variable declaration in gob-chunk's main method for
    the hash structure. As we can just reuse the function-global hash
    structure, there is no need to retain the shadowing variable at all.
    
    Remove the variable to fix the warning.

--- src/gob-chunk.c
+++ src/gob-chunk.c
@@ -46,8 +46,6 @@ int main(int argc, char *argv[])
         die("Unable to initialize hashing state");
 
     while ((bytes = read_bytes(STDIN_FILENO, block, BLOCK_LEN)) > 0) {
-        struct hash hash;
-
         total += (size_t) bytes;
 
         if (hash_state_update(&state, block, (size_t) bytes) < 0)
